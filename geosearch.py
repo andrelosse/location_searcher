99af81b8396fca82a18ca49db121ccf4ad45e814
Andre Losse <andreloze@gmail.com>
2022-08-16 20:40:58 -0300

    first version

---
+++ geosearch.py
@@ -0,0 +1,45 @@
+import json
+from geopy.geocoders import Nominatim
+
+
+def searchloc(search):
+
+    geolocator = Nominatim(user_agent="wazeyes")
+    location = geolocator.geocode(search)
+
+    infos = [location.address, location.latitude, location.longitude]
+    return infos
+
+
+def writesearch(search):
+
+    try:
+        with open("address.json", "w") as endjson:
+            json.dump(search, endjson)
+        print("Endereço gravado!")
+
+    except:
+        print("Cannot write file...")
+
+
+def readsearch():
+
+    try:
+        with open("address.json", "r") as newend:
+            search_loaded = json.load(newend)
+            return search_loaded
+
+    except FileNotFoundError:
+        print("File does not exit...")
+
+
+def writecoords(location):
+
+    try:
+
+        with open("coordinates.json", "w") as latlon:
+            json.dump(location, latlon)
+
+    except:
+
+        print("Cannot write coordinates on file...")
---
+++ main.py
@@ -0,0 +1,17 @@
+from geosearch import *
+
+
+def main():
+
+    writesearch(input("Address search -> "))
+
+    location = searchloc(readsearch())
+
+    print("Result -> " + str(location[0]))
+    print("Coordinates: -> " + str(location[1]) + " : " + str(location[2]))
+
+    writecoords(location)
+
+
+if __name__ == '__main__':
+    main()
